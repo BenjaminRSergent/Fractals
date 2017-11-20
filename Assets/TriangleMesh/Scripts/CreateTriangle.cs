43ce36aca099b559e3088bc85dbb028e313bfb96
Benjamin Sergent <benjamin.sergent@daqri.com>
2017-11-20 10:33:26 -0800

    Finished dynamic mesh

--- Assets/TriangleMesh/Scripts/CreateTriangle.cs
+++ Assets/TriangleMesh/Scripts/CreateTriangle.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CreateTriangle : MonoBehaviour
 {
+    public int numIterations = 4;
     private Mesh _mesh;
     private List<Vector3> _vertices = new List<Vector3>();
-    private int[] _indicies;
+    private int[] _indices;
 
     void Start()
     {
@@ -19,47 +21,59 @@ public class CreateTriangle : MonoBehaviour
         _vertices.Add(new Vector3(1, 0, 0));
         _mesh.SetVertices(_vertices);
 
-        _indicies = new int[3];
-        _indicies[0] = 0;
-        _indicies[1] = 1;
-        _indicies[2] = 2;
-        _mesh.SetIndices(_indicies, MeshTopology.Triangles, 0);
+        _indices = new int[3];
+        _indices[0] = 0;
+        _indices[1] = 1;
+        _indices[2] = 2;
+        _mesh.SetIndices(_indices, MeshTopology.Triangles, 0);
 
-        Iterate();
+        for(int iteration = 0; iteration < numIterations; iteration++)
+        {
+            Iterate();
+        }
+        
     }
 
     void Iterate()
     {
         var previousVerts = _vertices;
+        var previousIndices = _indices;
         _vertices = new List<Vector3>();
         int prevNumTriangles = previousVerts.Count / 2;
 
-        _vertices.Add(previousVerts[_indicies[0]]);
+        _indices = new int[prevNumTriangles * 9];
+
+        int indicesIndex = 0;
         for (int triangle = 0; triangle < prevNumTriangles; triangle++)
         {
             int indexOffset = triangle * 3;
-            Vector3 vertA = previousVerts[_indicies[indexOffset]];
-            Vector3 vertB = previousVerts[_indicies[indexOffset+1]];
-            Vector3 vertC = previousVerts[_indicies[indexOffset + 2]];
-            _vertices.Add((vertA + vertB) / 2.0f);
+
+            Vector3 vertA = previousVerts[previousIndices[indexOffset]];
+            Vector3 vertB = previousVerts[previousIndices[indexOffset + 1]];
+            Vector3 vertC = previousVerts[previousIndices[indexOffset + 2]];
+
+            int newVertexStart = _vertices.Count;
+
+            _vertices.Add(vertA);
             _vertices.Add(vertB);
-            _vertices.Add((vertB + vertC) / 2.0f);
             _vertices.Add(vertC);
             _vertices.Add((vertC + vertA) / 2.0f);
+            _vertices.Add((vertA + vertB) / 2.0f);
+            _vertices.Add((vertB + vertC) / 2.0f);
+            
+
+            _indices[indicesIndex++] = newVertexStart;
+            _indices[indicesIndex++] = newVertexStart + 4;
+            _indices[indicesIndex++] = newVertexStart + 3;
+            _indices[indicesIndex++] = newVertexStart + 4;
+            _indices[indicesIndex++] = newVertexStart + 1;
+            _indices[indicesIndex++] = newVertexStart + 5;
+            _indices[indicesIndex++] = newVertexStart + 5;
+            _indices[indicesIndex++] = newVertexStart + 2;
+            _indices[indicesIndex++] = newVertexStart + 3;
         }
         _mesh.SetVertices(_vertices);
-
-        int numTriangles = _vertices.Count / 2;
-        _indicies = new int[numTriangles*3];
-        for (int triangle = 0; triangle < numTriangles; triangle++)
-        {
-            int indexOffset = triangle * 3;
-            int vertexOffset = triangle * 2;
-            _indicies[indexOffset] = (vertexOffset + 1) % _vertices.Count;
-            _indicies[indexOffset + 1] = (vertexOffset + 2) % _vertices.Count;
-            _indicies[indexOffset + 2] = (vertexOffset + 3) % _vertices.Count;
-        }
-        _mesh.SetIndices(_indicies, MeshTopology.Triangles, 0);
+        _mesh.SetIndices(_indices, MeshTopology.Triangles, 0);
     }
 
     private void OnDrawGizmos()
@@ -71,8 +85,8 @@ public class CreateTriangle : MonoBehaviour
         Gizmos.color = Color.black;
         for (int index = 0; index < _vertices.Count; index++)
         {
-            Gizmos.DrawSphere(_vertices[index], 0.1f);
+            // Gizmos.DrawSphere(_vertices[index], 0.1f);
+            // Handles.Label(_vertices[index], "" + index);
         }
     }
-
 }
