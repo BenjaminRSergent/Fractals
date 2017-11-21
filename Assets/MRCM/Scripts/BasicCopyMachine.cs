82377f802f26b8f7f6cf914644b1b3a4968daf64
Benjamin Sergent <benjamin.sergent@daqri.com>
2017-11-21 07:10:21 -0800

    More comments

--- Assets/MRCM/Scripts/BasicCopyMachine.cs
+++ Assets/MRCM/Scripts/BasicCopyMachine.cs
@@ -15,9 +15,8 @@ public class BasicCopyMachine : MonoBehaviour {
     public int currentDepth;
     public Vector3 volume;
 
-    private Vector3 lowerLeft;
-
-    // Use this for initialization
+    private Vector3 _lowerLeft;
+    
     void Start()
     {
         Destroy(this);
@@ -27,28 +26,31 @@ public class BasicCopyMachine : MonoBehaviour {
         }
         if (currentDepth == 0)
         {
-            lowerLeft = transform.position - volume/2.0f;
+            // We're the first copy. Set the lower left reference point for all future copies.
+            _lowerLeft = transform.position - volume/2.0f;
         }
 
-
+        // The parents for all the copies we're about to make
         var newParent = new GameObject();
         newParent.transform.position = transform.position;
         newParent.transform.rotation = transform.rotation;
 
+        // Copy scale and translate the current object based on the machine lens
         foreach (Lens currLen  in lens)
         {
             var newObj = Instantiate(gameObject);
             newObj.transform.localScale = newObj.transform.localScale * currLen.scale;
-            newObj.transform.position = lowerLeft + new Vector3(volume.x * currLen.offset.x, volume.y * currLen.offset.y, volume.z * currLen.offset.z);
+            newObj.transform.position = _lowerLeft + new Vector3(volume.x * currLen.offset.x, volume.y * currLen.offset.y, volume.z * currLen.offset.z);
             newObj.transform.parent = newParent.transform;
         }
 
+        // Have the parent continue copying recursively
         var parentScript = newParent.AddComponent<BasicCopyMachine>();
         parentScript.lens = lens;
         parentScript.currentDepth = currentDepth+1;
         parentScript.maxDepth = maxDepth;
         parentScript.volume = volume;
-        parentScript.lowerLeft = lowerLeft;
+        parentScript._lowerLeft = _lowerLeft;
         Destroy(this.gameObject);
     }
 }
